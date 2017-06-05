bfbb3765218361dda88d06db428d317553e9cfbc
c9nguyen <cqnguyen21st@yahoo.com>
2017-06-05 03:54:56 -0700

    testing message sending

--- static/js/application.js
+++ static/js/application.js
@@ -32,6 +32,7 @@ $("#input-form").on("submit", function(event) {
   event.preventDefault();
   var handle = $("#input-handle")[0].value;
 //  var text   = $("#input-text")[0].value;
+console.log($("#input-text")[0].value);
   var text = "Hello";
   outbox.send(JSON.stringify({ handle: handle, text: text }));
   $("#input-text")[0].value = "";
