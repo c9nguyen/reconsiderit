66c6bba8f6b8e79dfacdf3293a55e131c4957190
Charlton Smith <charltonsmith@hotmail.com>
2017-06-05 03:39:38 -0700

    hey

--- chat.py
+++ chat.py
@@ -58,6 +58,7 @@ class ChatBackend(object):
         Automatically discards invalid connections."""
         try:
             client.send(data)
+            print "heyyyyyyyyyyyyyyyy"
         except Exception:
             self.clients.remove(client)
 
